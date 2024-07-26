ea347f30045b2e7acbfc9abb6b119a66104d7fbe
esgoet <toddesgo@gmail.com>
2024-07-26 09:57:37 +0200

    init

---
+++ src/main/java/com/github/esgoet/hhjava243todobackend/HhJava243TodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.esgoet.hhjava243todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava243TodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava243TodoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/esgoet/hhjava243todobackend/HhJava243TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.esgoet.hhjava243todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava243TodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
